b48e658790a8e1351fcd5ee1a16bbd85f75602aa
GrinderMode <157034279+GrinderMode@users.noreply.github.com>
2024-01-18 15:43:18 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat code as newer version of Javascript
+
+// alert( 3 + 3) // we are using node.js, not browser
+
+console.log(3+
+    3) // code readability is important
+
+console.log("Okasha")
+
+let name = "Okasha" //string
+let age = 16 //int
+let isLoggedIn = true //boolean
+let state; //undefined
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+// object
+
+console.log(typeof age) //to check data types
+console.log(typeof null)
